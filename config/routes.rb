04f116f070e9479e6239d487e348138066f300f1
aamharris <aamharris@gmail.com>
2015-04-21 23:51:16 -0500

    add custom rolldice route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   resources :welcome, only: [:index]
-  resources :game, only: [:index, :create, :current]
+  resources :game, only: [:index, :create]
+
+  get '/rolldice', to: 'game#rolldice', as: 'roll_dice'
    
 end
