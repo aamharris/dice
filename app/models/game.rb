1e769aeb3df50453898d537fabbc55648738301a
aamharris <aamharris@gmail.com>
2015-05-21 21:43:17 -0500

    add persisted and initialize method

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,15 @@
-class Game < ActiveRecord::Base
-  has_one :dice
+class Game
+  extend ActiveModel::Naming
+  include ActiveModel::Conversion
+  def persisted?
+    false
+  end
+
+  attr_accessor :players, :player_1, :player_2, :name, :players_turn
+
+  def initialize(players_turn)
+    @players = [player_1 = Player.new(name = "Player 1", Dice.new(6,2)),
+                player_2 = Player.new(name = "Player 2", Dice.new(6,2))]
+    @players_turn = players_turn
+  end
 end
