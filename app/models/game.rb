9195d49d48b5302b10e62432ba224cd7ab696a29
aamharris <aamharris@gmail.com>
2015-06-16 22:22:47 -0500

    remove players_turn argument

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,15 +1,16 @@
 class Game
   extend ActiveModel::Naming
   include ActiveModel::Conversion
-  def persisted?
-    false
-  end
+  # def persisted?
+  #   false
+  # end
 
   attr_accessor :players, :player_1, :player_2, :name, :players_turn
 
-  def initialize(players_turn)
+  def initialize
     @players = [player_1 = Player.new(name = "Player 1", Dice.new(6,2)),
                 player_2 = Player.new(name = "Player 2", Dice.new(6,2))]
-    @players_turn = players_turn
+
   end
+
 end
