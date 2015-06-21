e9f512dc8a2c61ef282b133e43eb19e919a62f92
aamharris <aamharris@gmail.com>
2015-06-21 17:46:33 -0500

    players are now in sessions to fix bug that changed names if multiple browsers were running game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,7 @@
 class GamesController < ApplicationController
   def index
-   @player = @@game.players[@@game.players_turn]    
+    @players_turn = session[:first].to_i
+    @player = session[:players][@players_turn]["name"]  
   end
 
   def new
@@ -18,12 +19,11 @@ class GamesController < ApplicationController
     else
       @goes_first_2 = @name_2
     end
-
   end
 
   def create
-    @@game = Game.new
     reset_session
+
     session[:player_1] = params[:player_1]  
     session[:player_2] = params[:player_2]
 
@@ -35,36 +35,48 @@ class GamesController < ApplicationController
       session[:player_2] = "Player 2"
     end
 
-    @@game.players_turn = params[:first].to_i
-    @@game.players[0].name = session[:player_1]
-    @@game.players[1].name = session[:player_2]
+    session[:players_turn] = params[:first].to_i
+
+    @players = [player_1 = Player.new(name = session[:player_1], Dice.new(6,2)),
+                player_2 = Player.new(name = session[:player_2], Dice.new(6,2))]
+
+    session[:players] = @players
 
     redirect_to games_path
   end
 
   def rolldice
-    @player = @@game.players[@@game.players_turn]
-    if @player.dice.rolls_left?
-      @roll = @player.dice.roll
+    @players_turn = session[:first].to_i
+    @player = session[:players][@players_turn]
+    @player_dice = session[:players][@players_turn]["dice"]["dice_roll_possibilities"]
+    @player_name = session[:players][@players_turn]["name"] 
+
+    if Dice.rolls_left?(@player_dice)
+      @roll = Dice.roll(@player_dice)
+      session[:players][@players_turn]["dice"]["dice_roll_possibilities"] = @player_dice
     else
-      @player.dice = Dice.new(6, 2)
-      rolldice
+      @dice = Dice.new_dice_set(6,2)
+      @roll = Dice.roll(@dice)
+      session[:players][@players_turn]["dice"]["dice_roll_possibilities"] = @dice
+           
     end
 
-    if @@game.players_turn == 0
-      @@game.players_turn += 1
+    if @players_turn == 0
+      @players_turn += 1
     else
-      @@game.players_turn = 0
+      @players_turn = 0
     end
-    next_player = @@game.players[@@game.players_turn]
 
+    session[:first] = @players_turn
+
+    next_player = session[:players][@players_turn]["name"]
+    next_player
+  
     @html_content = render_to_string
-    data = {:name => next_player.name, :html_content => @html_content}
-    view = 
+    data = {:name => next_player, :html_content => @html_content} 
 
     respond_to do |format|
       format.json { render :json => data,  :status => :ok }
-    end
-  
+    end  
   end
 end
--- app/models/dice.rb
+++ app/models/dice.rb
@@ -10,16 +10,28 @@ class Dice
     @dice_roll_possibilities.uniq!
   end
 
-  def rolls_left?
-    if @dice_roll_possibilities.length > 0
+  def self.rolls_left?(dice_arry)
+    if dice_arry.length > 0
       return true
     else
       return false
     end
   end
   
-  def roll
-    roll = @dice_roll_possibilities.sample
-    @dice_roll_possibilities.delete(roll)
+  def self.roll(dice_arry)
+    roll = dice_arry.sample
+    dice_arry.delete(roll)
+  end
+
+  def self.new_dice_set(sides, number_of_dice)
+    @sides = []
+    number_of_dice.times do
+      @sides << (1..sides).to_a
+    end
+
+    @sides.flatten!
+    @dice_roll_possibilities = @sides.combination(number_of_dice).to_a
+    return @dice_roll_possibilities.uniq!
+  
   end
 end
